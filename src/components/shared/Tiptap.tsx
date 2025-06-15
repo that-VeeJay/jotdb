9863dc33af8ce2f7835dccce47ae56698ea0a8f6
Vee Jay <114758021+that-VeeJay@users.noreply.github.com>
2025-06-15 23:33:02 +0800

    feat: add Placeholder extension to Tiptap editor

--- src/components/shared/Tiptap.tsx
+++ src/components/shared/Tiptap.tsx
@@ -1,15 +1,26 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
+import Placeholder from "@tiptap/extension-placeholder";
 import StarterKit from "@tiptap/starter-kit";
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      Placeholder.configure({ placeholder: "Write something..." }),
+      StarterKit,
+    ],
+    // content: "Hello World",
+    editorProps: {
+      attributes: {
+        class:
+          "scrollbar-hidden h-160 resize-none border rounded-md bg-neutral-100 dark:bg-neutral-900 p-3 overflow-y-auto",
+      },
+    },
   });
 
   return <EditorContent editor={editor} />;
 };
 
 export default Tiptap;
+
