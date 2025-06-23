39f47210b5e74fa4679afc83299477e984e505bf
Vee Jay <114758021+that-VeeJay@users.noreply.github.com>
2025-06-23 14:56:07 +0800

    feat: add theme toggle to auth layout component

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -13,49 +13,47 @@ import {
 
 export default function RegisterPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-3xl">Register</CardTitle>
-          <CardDescription>
-            Please provide all necessary information
-          </CardDescription>
-        </CardHeader>
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-3xl">Register</CardTitle>
+        <CardDescription>
+          Please provide all necessary information
+        </CardDescription>
+      </CardHeader>
 
-        <CardContent>
-          <form className="space-y-3">
-            <div className="space-y-1">
-              <Label htmlFor="display_name">Display name</Label>
-              <Input id="display_name" name="display_name" type="text" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" name="email" type="email" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" name="password" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="confirm_password">Confirm Password</Label>
-              <Input
-                id="confirm_password"
-                name="confirm_password"
-                type="password"
-              />
-            </div>
-            <Button className="w-full">Create account</Button>
-          </form>
-        </CardContent>
-        <CardFooter>
-          <span>
-            Already have an account?{" "}
-            <Link href="/login" className="font-semibold hover:underline">
-              Login.
-            </Link>
-          </span>
-        </CardFooter>
-      </Card>
-    </div>
+      <CardContent>
+        <form className="space-y-3">
+          <div className="space-y-1">
+            <Label htmlFor="display_name">Display name</Label>
+            <Input id="display_name" name="display_name" type="text" />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" name="email" type="email" />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" name="password" type="password" />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="confirm_password">Confirm Password</Label>
+            <Input
+              id="confirm_password"
+              name="confirm_password"
+              type="password"
+            />
+          </div>
+          <Button className="w-full">Create account</Button>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <span>
+          Already have an account?{" "}
+          <Link href="/login" className="font-semibold hover:underline">
+            Login.
+          </Link>
+        </span>
+      </CardFooter>
+    </Card>
   );
 }
