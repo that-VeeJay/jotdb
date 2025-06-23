529de9199d394932b05e5f06811ca6d5f6716680
Vee Jay <114758021+that-VeeJay@users.noreply.github.com>
2025-06-23 14:07:48 +0800

    chore: initial project setup
    - installed shadcn/ui component library
    - implemented barrel exports for UI components
    - added Poppins as a custom font

---
+++ src/components/ui/index.ts
@@ -0,0 +1,13 @@
+export { Button } from "./button";
+export { Input } from "./input";
+export { Label } from "./label";
+
+export {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
