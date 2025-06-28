11a120f42b30693f93785b58ff1ab44311185bbc
Vee Jay <114758021+that-VeeJay@users.noreply.github.com>
2025-06-28 16:38:47 +0800

    fix(layout): comment out unused script tag in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script
+        {/* <script
           crossOrigin="anonymous"
           src="//unpkg.com/react-scan/dist/auto.global.js"
-        />
+        /> */}
       </head>
       <body className={`${poppins.className} antialiased`}>
         <ThemeProvider
