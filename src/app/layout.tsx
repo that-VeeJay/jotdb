a48f133a659cf0b74e9faae4c06129e1960c7fb6
Vee Jay <114758021+that-VeeJay@users.noreply.github.com>
2025-07-16 14:35:01 +0800

    initial project setup
    - integrated shadcn/ui
    - implemented barrel file structure for UI components
    - cleaned up default Next.js boilerplate
    - added custom font

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "JOTDB",
+  description:
+    "A fast, simple, and organized note-taking app built for clarity and productivity.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
